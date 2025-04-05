4620bb692514b50714626ce46d80eecb009e690b
Shawn Thompson <sthompson7720@gmail.com>
2025-04-04 21:09:36 -0400

    Added further AccountingPage layout with placeholder content and an image for improved presentation.

--- src/app/services/accounting/page.js
+++ src/app/services/accounting/page.js
@@ -1,12 +1,81 @@
 import NavBar from "@/components/Navbar";
 import PageHeader from "@/components/PageHeader";
+import Image from "next/image";
+
+import photo from "../../../../public/DeannaT.webp";
 
 export default function AccountingPage() {
   return (
     <div>
       <NavBar />
       <PageHeader previousPage="Home" currentPage="Accounting" />
-      <main className="p-4">{/* Add more content here */}</main>
+      <main className="md:p-12 p-5 flex flex-col md:flex-row md:space-x-12">
+        <div id="col-1" className="basis-full md:basis-1/2 order-1 md:order-1">
+          <h2 className="font-bold text-2xl mb-4">
+            Specialized Small Business Accounting for Real Estate
+          </h2>
+          <h3 className="text-lg mb-4">
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+            eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+            Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim
+            in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id
+            cursus faucibus, tortor neque egestas augue, eu vulputate magna eros
+            eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan
+            porttitor, facilisis luctus, metus
+          </h3>
+          <h2 className="font-bold text-2xl mb-4">
+            Specialized Small Business Accounting for Real Estate
+          </h2>
+          <h3 className="text-lg mb-4">
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+            eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+            Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim
+            in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id
+            cursus faucibus, tortor neque egestas augue, eu vulputate magna eros
+            eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan
+            porttitor, facilisis luctus, metus
+          </h3>
+          <h2 className="font-bold text-2xl mb-4">
+            Specialized Small Business Accounting for Real Estate
+          </h2>
+          <h3 className="text-lg mb-4">
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+            eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+            Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit
+            amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros
+            ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim
+            in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id
+            cursus faucibus, tortor neque egestas augue, eu vulputate magna eros
+            eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan
+            porttitor, facilisis luctus, metus
+          </h3>
+        </div>
+        <div
+          id="col-2"
+          className="basis-full md:basis-1/2 flex justify-center items-start order-2 md:order-2"
+        >
+          <Image
+            src={photo}
+            alt="Photo"
+            width={500}
+            height={500}
+            className="rounded md:block hidden"
+          />
+        </div>
+      </main>
     </div>
   );
 }
