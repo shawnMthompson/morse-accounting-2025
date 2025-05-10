53cab81d2688db98c0024f6c63e62f7d330f354a
Shawn Thompson <sthompson7720@gmail.com>
2025-05-10 04:06:59 -0400

    Removed the about page from the website, opting to house that content on the home page.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -72,14 +72,6 @@ export default function Navbar() {
                 Home
               </Link>
             </li>
-            <li>
-              <Link
-                href="/about"
-                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </Link>
-            </li>
             {/* Services Dropdown Toggle */}
             <li className="relative" ref={dropdownRef}>
               <button
