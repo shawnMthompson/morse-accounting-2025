01d0835f0591d168621d4899af64453c1b666c5c
Shawn Thompson <sthompson7720@gmail.com>
2025-05-22 01:45:18 -0400

    Designed the GetInTouch component to enhance layout and user engagement with detailed messaging and a contact button.

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -1,8 +1,24 @@
 // Get in Touch (Leads to Contact Page)
+import Link from "next/link";
+
 export default function GetInTouch() {
   return (
-    <>
-      <p>Get In Touch</p>
-    </>
+    <div className="flex flex-col justify-center items-center mx-auto w-1/2 space-y-2 mt-10 mb-10">
+      <h5 className="text-2xl text-center font-bold">GET IN TOUCH</h5>
+      <p className="text-center">
+        We are eager to discuss your business needs, and answer any questions
+        that you may have. Enter your details at the contact section of our
+        website and we&apos;ll get back to you as soon as we can!
+      </p>
+      <hr></hr>
+      <Link href="/contact">
+        <button
+          type="button"
+          className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
+        >
+          Pass us a Message
+        </button>
+      </Link>
+    </div>
   );
 }
